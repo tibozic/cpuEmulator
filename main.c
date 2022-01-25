728f27e80e684eb304274d4c1c86107f2b42a542
Timotej Božič <timotejbozic.tb@gmail.com>
2022-01-25 23:35:39 +0100

    Implemented the LDA_ZP and LDA_ZPX instructions.
    Also created the lda_set_flags() function, which sets the
    appropriate flags when loading to A register.

--- main.c
+++ main.c
@@ -39,14 +39,18 @@ typedef struct
 } CPU;
 
 // Opcodes
-#define LDA_IM 0xA9
+#define INS_LDA_IM 0xA9
+#define INS_LDA_ZP 0xA5
+#define INS_LDA_ZPX 0xB5
 #define END 0x0
 
 // Functions
 void reset_cpu(CPU *cpu, MEMORY *memory);
 void initialise_memory(MEMORY *memory);
 void execute_instruction(int clock, CPU *cpu, MEMORY *memory);
-BYTE fetch(int *clock, CPU *cpu, MEMORY *memory);
+BYTE fetch_byte(int *clock, CPU *cpu, MEMORY *memory);
+BYTE read_byte(int *clock, BYTE address, MEMORY *memory);
+void lda_set_flags(CPU *cpu);
 
 int main(void)
 {
@@ -55,9 +59,15 @@ int main(void)
 
 	reset_cpu(&cpu, &memory);
 
-	memory.data[0xFFFC] = 0x00A9;
-	memory.data[0xFFFD] = 13;
+	/* Start of simple test program */
+	memory.data[0xFFFC] = INS_LDA_ZPX;
+	memory.data[0xFFFD] = 0x42;
+	cpu.x = 0x01;
+	memory.data[0x43] = 0x23;
 	execute_instruction(3, &cpu, &memory);
+	/* End of simple test program */
+
+	printf("Value in a: %d\n", cpu.a);
 
 	return 0;
 }
@@ -84,10 +94,11 @@ void initialise_memory(MEMORY *memory)
 void execute_instruction(int clock, CPU *cpu, MEMORY *memory)
 {
 	BYTE instruction;
+	BYTE zp_address;
 
 	while (clock > 0)
 	{
-		instruction = fetch(&clock, cpu, memory);
+		instruction = fetch_byte(&clock, cpu, memory);
 		switch(instruction)
 		{
 			case END:
@@ -96,22 +107,68 @@ void execute_instruction(int clock, CPU *cpu, MEMORY *memory)
 				clock = 0;
 				break;
 			}
-			case LDA_IM:
+			case INS_LDA_IM:
 			{
-				cpu->a = fetch(&clock, cpu, memory);
+				cpu->a = fetch_byte(&clock, cpu, memory);
 				cpu->z = (cpu->a == 0);
 				// Check if 7th bit of cpu->a is 1 (number is negative)
 				cpu->n = ((cpu->a & (1 << 6)) > 0);
+				break;
+			}
+			case INS_LDA_ZP:
+			{
+				zp_address = fetch_byte(&clock, cpu, memory);
+
+				cpu->a = read_byte(&clock, zp_address, memory);
+				lda_set_flags(cpu);
+
+				break;
+			}
+			case INS_LDA_ZPX:
+			{
+				zp_address = fetch_byte(&clock, cpu, memory);
+
+				zp_address += cpu->x;
+				clock--;
+
+				cpu->a = read_byte(&clock, zp_address, memory);
+				lda_set_flags(cpu);
+
 				break;
 			}
 		}
 	}
 }
 
-BYTE fetch(int *clock, CPU *cpu, MEMORY *memory)
+BYTE fetch_byte(int *clock, CPU *cpu, MEMORY *memory)
 {
+	/*
+	 * Fetches a byte from memory and uses a clock cycle
+	 * Also increments the program counter
+	*/
 	BYTE data = memory->data[cpu->pc];
 	cpu->pc++;
-	*clock = *clock - 1;
+	*clock -= 1;
 	return data;
+}
+
+BYTE read_byte(int *clock, BYTE address, MEMORY *memory)
+{
+	/*
+	 * Fetches a byte from memory and uses a clock cylce
+	 * Does NOT increment the program counter
+	*/
+	BYTE data = memory->data[address];
+	*clock -= - 1;
+	return data;
+}
+
+void lda_set_flags(CPU *cpu)
+{
+	/*
+	 * Sets the appropriate flagsa when loading to cpu->a register
+	*/
+	cpu->z = (cpu->a == 0);
+	// Check if 7th bit of cpu->a is 1 (number is negative)
+	cpu->n = ((cpu->a & (1 << 6)) > 0);
 }

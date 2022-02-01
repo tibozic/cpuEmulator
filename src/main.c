3a3706aa25eabd686664b0611b501f36503fe371
Timotej Božič <timotejbozic.tb@gmail.com>
2022-02-01 12:28:30 +0100

    Added tests for INS_LDA_ABSX.

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 void test_lda_im(CPU cpu, MEMORY memory);
 void test_lda_zp(CPU cpu, MEMORY memory);
 void test_lda_zpx(CPU cpu, MEMORY memory);
+void test_lda_abs(CPU cpu, MEMORY memory);
+void test_lda_absx(CPU cpu, MEMORY memory);
 
 int main(void)
 {
@@ -14,6 +16,7 @@ int main(void)
 	test_lda_zp(cpu, memory);
 	test_lda_zpx(cpu, memory);
 	test_lda_abs(cpu, memory);
+	test_lda_absx(cpu, memory);
 	print_report();
 }
 
@@ -216,4 +219,109 @@ void test_lda_abs(CPU cpu, MEMORY memory)
 	END_TEST();
 }
 
+void test_lda_absx(CPU cpu, MEMORY memory)
+{
+	int number_of_instructions;
+
+	START_TEST("LDA Absolute X");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0x3;
+	memory.data[0xFFFC] = INS_LDA_ABSX;
+	memory.data[0xFFFD] = 0x12;
+	memory.data[0xFFFE] = 0x0;
+	memory.data[0x15] = 0x15;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.a, 0x15);
+	EXPECT_EQ(number_of_instructions, 4);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 0);
+
+	END_TEST();
+
+	START_TEST("LDA Absolute X - negative");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0x3;
+	memory.data[0xFFFC] = INS_LDA_ABSX;
+	memory.data[0xFFFD] = 0x12;
+	memory.data[0xFFFE] = 0x0;
+	memory.data[0x15] = 0x42;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.a, 0x42);
+	EXPECT_EQ(number_of_instructions, 4);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 1);
+
+	END_TEST();
+
+	START_TEST("LDA Absolute X - zero");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0x3;
+	memory.data[0xFFFC] = INS_LDA_ABSX;
+	memory.data[0xFFFD] = 0x12;
+	memory.data[0xFFFE] = 0x0;
+	memory.data[0x15] = 0x0;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.a, 0x0);
+	EXPECT_EQ(number_of_instructions, 4);
+	EXPECT_EQ(cpu.z, 1);
+	EXPECT_EQ(cpu.n, 0);
+
+	END_TEST();
+
+	START_TEST("LDA Absolute X page crossed");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0xF;
+	memory.data[0xFFFC] = INS_LDA_ABSX;
+	memory.data[0xFFFD] = 0xFF;
+	memory.data[0xFFFE] = 0x0;
+	memory.data[0x010E] = 0x15;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.a, 0x15);
+	EXPECT_EQ(number_of_instructions, 5);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 0);
+
+	END_TEST();
+
+	START_TEST("LDA Absolute X page crossed - negative");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0xF;
+	memory.data[0xFFFC] = INS_LDA_ABSX;
+	memory.data[0xFFFD] = 0xFF;
+	memory.data[0xFFFE] = 0x0;
+	memory.data[0x010E] = 0x42;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.a, 0x42);
+	EXPECT_EQ(number_of_instructions, 5);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 1);
+
+	END_TEST();
+
+	START_TEST("LDA Absolute X page crossed- zero");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0xF;
+	memory.data[0xFFFC] = INS_LDA_ABSX;
+	memory.data[0xFFFD] = 0xFF;
+	memory.data[0xFFFE] = 0x0;
+	memory.data[0x010E] = 0x0;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.a, 0x0);
+	EXPECT_EQ(number_of_instructions, 5);
+	EXPECT_EQ(cpu.z, 1);
+	EXPECT_EQ(cpu.n, 0);
+
+	END_TEST();
 }

623c543cf51992d5f8d6c457fe3e7dac44a96a75
Timotej Božič <timotejbozic.tb@gmail.com>
2022-02-02 12:48:29 +0100

    Added tests for all the INS_LDY instructions.

--- src/main.c
+++ src/main.c
@@ -16,6 +16,12 @@ void test_ldx_zpy(CPU cpu, MEMORY memory);
 void test_ldx_abs(CPU cpu, MEMORY memory);
 void test_ldx_absy(CPU cpu, MEMORY memory);
 
+void test_ldy_im(CPU cpu, MEMORY memory);
+void test_ldy_zp(CPU cpu, MEMORY memory);
+void test_ldy_zpx(CPU cpu, MEMORY memory);
+void test_ldy_abs(CPU cpu, MEMORY memory);
+void test_ldy_absx(CPU cpu, MEMORY memory);
+
 int main(void)
 {
 	CPU cpu;
@@ -35,6 +41,12 @@ int main(void)
 	test_ldx_zpy(cpu, memory);
 	test_ldx_abs(cpu, memory);
 	test_ldx_absy(cpu, memory);
+
+	test_ldy_im(cpu, memory);
+	test_ldy_zp(cpu, memory);
+	test_ldy_zpx(cpu, memory);
+	test_ldy_abs(cpu, memory);
+	test_ldy_absx(cpu, memory);
 
 	print_report();
 }
@@ -877,7 +889,7 @@ void test_ldx_absy(CPU cpu, MEMORY memory)
 
 	END_TEST();
 
-	START_TEST("LDX Absolute Y page crossed- zero");
+	START_TEST("LDX Absolute Y page crossed - zero");
 
 	reset_cpu(&cpu, &memory);
 	cpu.y = 0xF;
@@ -888,6 +900,312 @@ void test_ldx_absy(CPU cpu, MEMORY memory)
 	number_of_instructions = execute_instruction(&cpu, &memory);
 
 	EXPECT_EQ(cpu.x, 0x0);
+	EXPECT_EQ(number_of_instructions, 5);
+	EXPECT_EQ(cpu.z, 1);
+	EXPECT_EQ(cpu.n, 0);
+
+	END_TEST();
+}
+
+void test_ldy_im(CPU cpu, MEMORY memory)
+{
+	int number_of_instructions;
+
+	START_TEST("LDY Immediate");
+
+	reset_cpu(&cpu, &memory);
+	memory.data[0xFFFC] = INS_LDY_IM;
+	memory.data[0xFFFD] = 0x12;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x12);
+	EXPECT_EQ(number_of_instructions, 2);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 0);
+	END_TEST();
+
+	START_TEST("LDY Immediate - negative");
+
+	reset_cpu(&cpu, &memory);
+	memory.data[0xFFFC] = INS_LDY_IM;
+	memory.data[0xFFFD] = 0x42;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x42);
+	EXPECT_EQ(number_of_instructions, 2);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 1);
+	END_TEST();
+
+	START_TEST("LDY Immediate - zero");
+
+	reset_cpu(&cpu, &memory);
+	memory.data[0xFFFC] = INS_LDY_IM;
+	memory.data[0xFFFD] = 0x0;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x0);
+	EXPECT_EQ(number_of_instructions, 2);
+	EXPECT_EQ(cpu.z, 1);
+	EXPECT_EQ(cpu.n, 0);
+	END_TEST();
+}
+
+void test_ldy_zp(CPU cpu, MEMORY memory)
+{
+	int number_of_instructions;
+	START_TEST("LDY Zero page");
+
+	reset_cpu(&cpu, &memory);
+	memory.data[0xFFFC] = INS_LDY_ZP;
+	memory.data[0xFFFD] = 0x42;
+	memory.data[0x42]	= 0x12;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x12);
+	EXPECT_EQ(number_of_instructions, 3);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 0);
+
+	END_TEST();
+
+	START_TEST("LDY Zero page - negative");
+
+	reset_cpu(&cpu, &memory);
+	memory.data[0xFFFC] = INS_LDY_ZP;
+	memory.data[0xFFFD] = 0x42;
+	memory.data[0x42]	= 0x42;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x42);
+	EXPECT_EQ(number_of_instructions, 3);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 1);
+
+	END_TEST();
+
+	START_TEST("LDY Zero page - zero");
+
+	reset_cpu(&cpu, &memory);
+	memory.data[0xFFFC] = INS_LDY_ZP;
+	memory.data[0xFFFD] = 0x42;
+	memory.data[0x42]	= 0x0;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x0);
+	EXPECT_EQ(number_of_instructions, 3);
+	EXPECT_EQ(cpu.z, 1);
+	EXPECT_EQ(cpu.n, 0);
+
+	END_TEST();
+}
+
+void test_ldy_zpx(CPU cpu, MEMORY memory)
+{
+	int number_of_instructions;
+
+	START_TEST("LDY  Zero page X");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0x7;
+	memory.data[0xFFFC] = INS_LDY_ZPX;
+	memory.data[0xFFFD] = 0x42;
+	memory.data[0x49] = 0x13;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x13);
+	EXPECT_EQ(number_of_instructions, 4);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 0);
+
+	END_TEST();
+
+	START_TEST("LDY  Zero page X - negative");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0x7;
+	memory.data[0xFFFC] = INS_LDY_ZPX;
+	memory.data[0xFFFD] = 0x42;
+	memory.data[0x49] = 0x42;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x42);
+	EXPECT_EQ(number_of_instructions, 4);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 1);
+
+	END_TEST();
+
+	START_TEST("LDY  Zero page X - zero");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0x7;
+	memory.data[0xFFFC] = INS_LDY_ZPX;
+	memory.data[0xFFFD] = 0x42;
+	memory.data[0x49] = 0x0;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x0);
+	EXPECT_EQ(number_of_instructions, 4);
+	EXPECT_EQ(cpu.z, 1);
+	EXPECT_EQ(cpu.n, 0);
+
+	END_TEST();
+}
+
+void test_ldy_abs(CPU cpu, MEMORY memory)
+{
+	int number_of_instructions;
+
+	START_TEST("LDY Absolute");
+
+	reset_cpu(&cpu, &memory);
+	memory.data[0xFFFC] = INS_LDY_ABS;
+	memory.data[0xFFFD] = 0x42;
+	memory.data[0xFFFE] = 0x12;
+	memory.data[0x1242] = 0x15;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x15);
+	EXPECT_EQ(number_of_instructions, 4);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 0);
+
+	END_TEST();
+
+	START_TEST("LDY Absolute - negative");
+
+	reset_cpu(&cpu, &memory);
+	memory.data[0xFFFC] = INS_LDY_ABS;
+	memory.data[0xFFFD] = 0x42;
+	memory.data[0xFFFE] = 0x12;
+	memory.data[0x1242] = 0x42;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x42);
+	EXPECT_EQ(number_of_instructions, 4);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 1);
+
+	END_TEST();
+
+	START_TEST("LDY Absolute - zero");
+
+	reset_cpu(&cpu, &memory);
+	memory.data[0xFFFC] = INS_LDY_ABS;
+	memory.data[0xFFFD] = 0x42;
+	memory.data[0xFFFE] = 0x12;
+	memory.data[0x1242] = 0x0;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x0);
+	EXPECT_EQ(number_of_instructions, 4);
+	EXPECT_EQ(cpu.z, 1);
+	EXPECT_EQ(cpu.n, 0);
+
+	END_TEST();
+}
+
+void test_ldy_absx(CPU cpu, MEMORY memory)
+{
+	int number_of_instructions;
+
+	START_TEST("LDY Absolute X");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0x3;
+	memory.data[0xFFFC] = INS_LDY_ABSX;
+	memory.data[0xFFFD] = 0x12;
+	memory.data[0xFFFE] = 0x0;
+	memory.data[0x15] = 0x15;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x15);
+	EXPECT_EQ(number_of_instructions, 4);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 0);
+
+	END_TEST();
+
+	START_TEST("LDY Absolute X - negative");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0x3;
+	memory.data[0xFFFC] = INS_LDY_ABSX;
+	memory.data[0xFFFD] = 0x12;
+	memory.data[0xFFFE] = 0x0;
+	memory.data[0x15] = 0x42;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x42);
+	EXPECT_EQ(number_of_instructions, 4);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 1);
+
+	END_TEST();
+
+	START_TEST("LDY Absolute X - zero");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0x3;
+	memory.data[0xFFFC] = INS_LDY_ABSX;
+	memory.data[0xFFFD] = 0x12;
+	memory.data[0xFFFE] = 0x0;
+	memory.data[0x15] = 0x0;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x0);
+	EXPECT_EQ(number_of_instructions, 4);
+	EXPECT_EQ(cpu.z, 1);
+	EXPECT_EQ(cpu.n, 0);
+
+	END_TEST();
+
+	START_TEST("LDY Absolute X page crossed");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0xF;
+	memory.data[0xFFFC] = INS_LDY_ABSX;
+	memory.data[0xFFFD] = 0xFF;
+	memory.data[0xFFFE] = 0x0;
+	memory.data[0x010E] = 0x15;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x15);
+	EXPECT_EQ(number_of_instructions, 5);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 0);
+
+	END_TEST();
+
+	START_TEST("LDY Absolute X page crossed - negative");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0xF;
+	memory.data[0xFFFC] = INS_LDY_ABSX;
+	memory.data[0xFFFD] = 0xFF;
+	memory.data[0xFFFE] = 0x0;
+	memory.data[0x010E] = 0x42;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x42);
+	EXPECT_EQ(number_of_instructions, 5);
+	EXPECT_EQ(cpu.z, 0);
+	EXPECT_EQ(cpu.n, 1);
+
+	END_TEST();
+
+	START_TEST("LDY Absolute X page crossed - zero");
+
+	reset_cpu(&cpu, &memory);
+	cpu.x = 0xF;
+	memory.data[0xFFFC] = INS_LDY_ABSX;
+	memory.data[0xFFFD] = 0xFF;
+	memory.data[0xFFFE] = 0x0;
+	memory.data[0x010E] = 0x0;
+	number_of_instructions = execute_instruction(&cpu, &memory);
+
+	EXPECT_EQ(cpu.y, 0x0);
 	EXPECT_EQ(number_of_instructions, 5);
 	EXPECT_EQ(cpu.z, 1);
 	EXPECT_EQ(cpu.n, 0);

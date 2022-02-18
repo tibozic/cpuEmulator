c0016eed6dcb430ec43727a9401bbca0b5f197bd
Timotej Božič <timotejbozic.tb@gmail.com>
2022-02-18 23:13:54 +0100

    Implemented INS_AND_INDX.

--- src/cpu.c
+++ src/cpu.c
@@ -584,6 +584,23 @@ int instruction_execute(CPU *cpu, MEMORY *memory)
 
 				break;
 			}
+			case INS_AND_INDX:
+			{
+				zp_address = byte_fetch(&clock, cpu, memory);
+
+				zp_addressx = zp_address + cpu->x;
+				clock++;
+
+				abs_address = word_read(&clock, zp_addressx, memory);
+
+				temp_data = byte_read(&clock, abs_address, memory);
+
+				cpu->a &= temp_data;
+
+				cpu_ld_set_flags(cpu, cpu->a);
+
+				break;
+			}
 			case INS_JSR:
 			{
 				abs_address = word_fetch(&clock, cpu, memory);

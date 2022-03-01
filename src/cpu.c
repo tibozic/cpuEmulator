bd42f392c8c0e53aac57adc25694672ba984ec89
tibozic <timotejbozic.tb@gmail.com>
2022-03-01 09:39:02 +0100

    Implemented INS_EOR_INDY.

--- src/cpu.c
+++ src/cpu.c
@@ -728,6 +728,27 @@ int instruction_execute(CPU *cpu, MEMORY *memory)
 
 				break;
 			}
+			case INS_EOR_INDY:
+			{
+				zp_address = byte_fetch(&clock, cpu, memory);
+
+				abs_address = word_read(&clock, zp_address, memory);
+
+				abs_addressx = abs_address + cpu->y;
+				clock++;
+
+				crossed_page_boundry = (abs_address ^ abs_addressx) >> 8;
+				if ( !crossed_page_boundry )
+				{
+					clock--;
+				}
+
+				cpu->a ^= byte_read(&clock, abs_addressx, memory);
+
+				cpu_ld_set_flags(cpu, cpu->a);
+
+				break;
+			}
 			case INS_JSR:
 			{
 				abs_address = word_fetch(&clock, cpu, memory);

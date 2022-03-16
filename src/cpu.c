22637c656a2441305d861ede6727fb02f077b363
tibozic <timotejbozic.tb@gmail.com>
2022-03-16 22:47:25 +0100

    Implemented INS_ORA_ABSX.

--- src/cpu.c
+++ src/cpu.c
@@ -820,6 +820,27 @@ int instruction_execute(CPU *cpu, MEMORY *memory)
 
 				break;
 			}
+			case INS_ORA_ABSX:
+			{
+				abs_address = word_fetch(&clock, cpu, memory);
+
+				abs_addressx = abs_address + cpu->x;
+				clock++;
+
+				crossed_page_boundry = (abs_address ^ abs_addressx) >> 8;
+				if ( !crossed_page_boundry )
+				{
+					clock--;
+				}
+
+				temp_data = byte_read(&clock, abs_addressx, memory);
+
+				cpu->a |= temp_data;
+
+				cpu_ld_set_flags(cpu, cpu->a);
+
+				break;
+			}
 			case INS_JSR:
 			{
 				abs_address = word_fetch(&clock, cpu, memory);
